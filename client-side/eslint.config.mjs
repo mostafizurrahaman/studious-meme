62a6922dce6372fe0dc8f24560978c5ac67f0802
Khaled Siddique <khaledssbd@gmail.com>
2026-04-14 10:02:51 +0600

    updated

--- client-side/eslint.config.mjs
+++ client-side/eslint.config.mjs
@@ -3,22 +3,22 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // {
-  //   rules: {
-  //     'react/display-name': 'off',
-  //   },
-  // },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-    // ".opencode/**",
-  ]),
+    ...nextVitals,
+    ...nextTs,
+    // {
+    //   rules: {
+    //     'react/display-name': 'off',
+    //   },
+    // },
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        '.next/**',
+        'out/**',
+        'build/**',
+        'next-env.d.ts',
+        // ".opencode/**",
+    ]),
 ]);
 
 export default eslintConfig;
