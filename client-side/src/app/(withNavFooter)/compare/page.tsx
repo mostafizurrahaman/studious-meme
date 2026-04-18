bfecb2f838395c4a7e670d7c71a1bac38993cf98
khaledssbd <khaledssbd@gmail.com>
2026-04-18 21:31:00 +0600

    updated

--- client-side/src/app/(withNavFooter)/compare/page.tsx
+++ client-side/src/app/(withNavFooter)/compare/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-
 import { Card, CardContent } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { featuredProducts } from '@/lib/malamal-content';
