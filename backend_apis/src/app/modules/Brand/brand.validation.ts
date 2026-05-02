4d68265c0bea71cdab1951064e60d4c10bfcb331
khaledssbd <khaledssbd@gmail.com>
2026-05-02 18:10:49 +0600

    brang slug length is now 3 -> 2

--- backend_apis/src/app/modules/Brand/brand.validation.ts
+++ backend_apis/src/app/modules/Brand/brand.validation.ts
@@ -5,12 +5,12 @@ const brandBaseSchema = z.object({
   name: z
     .string({ error: 'Name is required' })
     .trim()
-    .min(3, { message: 'Name must be at least 3 characters long' })
+    .min(2, { message: 'Name must be at least 2 characters long' })
     .max(50, { message: 'Name must be at most 50 characters long' }),
   slug: z
     .string({ error: 'Slug is required' })
     .trim()
-    .min(3, { message: 'Slug must be at least 3 characters long' })
+    .min(2, { message: 'Slug must be at least 2 characters long' })
     .max(50, { message: 'Slug must be at most 50 characters long' }),
   image: z.string().optional(),
   description: z
@@ -27,7 +27,7 @@ export const BrandValidation = {
       slug: z
         .string({ error: 'Slug is required' })
         .trim()
-        .min(3, { message: 'Slug must be at least 3 characters long' }),
+        .min(2, { message: 'Slug must be at least 2 characters long' }),
     }),
     body: brandBaseSchema.partial().extend({
       description: brandBaseSchema.shape.description,
