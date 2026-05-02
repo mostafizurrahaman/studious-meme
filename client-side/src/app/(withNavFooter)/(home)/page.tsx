b013e798f6e24883f890a2397daeec3cf62db47e
khaledssbd <khaledssbd@gmail.com>
2026-05-02 19:14:07 +0600

    updated

--- client-side/src/app/(withNavFooter)/(home)/page.tsx
+++ client-side/src/app/(withNavFooter)/(home)/page.tsx
@@ -12,8 +12,6 @@ export default async function Page() {
   const heroContent = await getHomeContent().catch(() => null);
   const content = heroContent?.data ?? null;
 
-  console.dir({ content }, {depth:null});
-
   const [schemaProducts, schemaBrands] = await Promise.all([
     content?.featuredProducts?.length || content?.latestProducts?.length
       ? Promise.all(
