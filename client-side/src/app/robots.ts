4c778e933ba37efb4b12f32a2d3c8591446d9df0
khaledssbd <khaledssbd@gmail.com>
2026-05-18 20:29:52 +0600

    updated

--- client-side/src/app/robots.ts
+++ client-side/src/app/robots.ts
@@ -3,11 +3,24 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/my-account', '/checkout', '/cart', '/wishlist', '/compare'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/dashboard',
+          '/my-account',
+          '/checkout',
+          '/cart',
+          '/wishlist',
+          '/admin',
+          '/super-admin',
+          '/payment',
+          '/_next',
+          '/api',
+        ],
+      },
+    ],
     sitemap: absoluteUrl('/sitemap.xml'),
   };
 }
